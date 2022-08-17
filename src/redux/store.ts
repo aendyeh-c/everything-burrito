1950f0b52af3e6d611a57abc8148b7560123007e
Andy Valjakka <110393708+aendyeh-c@users.noreply.github.com>
2022-08-17 09:30:10 +0300

    Include reducer in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from './reducers/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    counter: counterReducer,
   },
 });
 
